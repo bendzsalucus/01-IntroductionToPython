1f1aefcd42ff54b78665438755afbc46a8660931
bendzslt <bendzslt@rose-hulman.edu>
2018-08-30 08:33:45 -0400

    Updated to fix product from sum.

--- src/m2_todo_and_commit_push.py
+++ src/m2_todo_and_commit_push.py
@@ -1,6 +1,6 @@
 print('Hello, World')
 print('I love Rose-Hulman!')
-print( 3607 + 34227)
+print(3607 * 34227)
 ###############################################################################
 #
 #   This line is a COMMENT -- a note to human readers of this file.
